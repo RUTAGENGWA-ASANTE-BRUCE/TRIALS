4d0ae9afd9545cf80c093ad6f4b76fdd3aabfb78
RUTAGENGWA-ASANTE-BRUCE <96545712+RUTAGENGWA-ASANTE-BRUCE@users.noreply.github.com>
2022-05-19 15:55:33 +0200

    new changes

---
+++ selling/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Conditions from './Conditions';
+import House from './House';
+import ExpenseItem from "./components/ExpenseItem"
+
+function App ()
+{
+  return (
+    <div className=' '>
+      <ExpenseItem />
+    </div>
+  );
+}
+
+export default App;
