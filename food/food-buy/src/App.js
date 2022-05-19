217fd3a7beab4579798cefb5e85629031844b3d0
RUTAGENGWA-ASANTE-BRUCE <96545712+RUTAGENGWA-ASANTE-BRUCE@users.noreply.github.com>
2022-05-19 15:53:09 +0200

    new changes

---
+++ food/food-buy/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import FirstGroup from './FirstGroup';
+import SecondGroup from './SecondGroup';
+
+function App ()
+{
+  return (
+    <div className="App bg-gray-350 m-0 flex-row flex">
+      <FirstGroup />
+      <SecondGroup />
+    </div>
+  );
+}
+
+export default App;
